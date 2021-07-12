ff1137fafdb2109d81261e429ae11dfee62878f0
sadalmelik <86509799+wh-sadalmelik@users.noreply.github.com>
2021-07-12 23:26:37 +0800

    switch to single quote

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import i18n from "./i18n";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import i18n from './i18n';
 
 // Create App
-createApp(App).use(i18n).use(store).use(router).mount("#app");
+createApp(App).use(i18n).use(store).use(router).mount('#app');
